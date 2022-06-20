3237bfe296a4713e089afe3e6f54e5ae849a77c2
Nathan Bain <nate.bain9@gmail.com>
2022-06-19 21:24:47 -0400

    package.json and try to import variables to helpers

--- helpers/scheduleComparison.js
+++ helpers/scheduleComparison.js
@@ -1,17 +1,22 @@
-function scheduleComparison(arr1, arr2){
-  if (arr1.length !== arr2.length) return false;
-  for (var i = 0, len = arr1.length; i < len; i++){
-      if (arr1[i] !== arr2[i]){
-          return false;
-      }
-  }
-  return true; 
-}
+import {ANAsched} from '../db/anaheim-ducks/sched.js'
 
-const scheduleComparison = (date, a, b) => {
-  var i = 0, len = myArray.length;
-    while (i < len) {
-        // your code
-        i++
-    }
-}
+
+// function scheduleComparison(arr1, arr2){
+//   if (arr1.length !== arr2.length) return false;
+//   for (var i = 0, len = arr1.length; i < len; i++){
+//       if (arr1[i] !== arr2[i]){
+//           return false;
+//       }
+//   }
+//   return true; 
+// }
+
+// const scheduleComparison = (date, a, b) => {
+//   var i = 0, len = myArray.length;
+//     while (i < len) {
+//         // your code
+//         i++
+//     }
+// }
+
+console.log(ANAsched)
