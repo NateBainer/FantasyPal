26d2bf4dd61d2d51153f91ca5a2c7daac4f935d9
Nathan Bain <nate.bain9@gmail.com>
2022-06-22 12:28:44 -0400

    fixed some routing on backend and transfered all html and css back to main file

---
+++ README.md
@@ -0,0 +1,63 @@
+# FantasyPal ReadMe
+  
+  FantasyPal is an app designed to maximize your potential scoring output for your fantasy hockey team! 
+  Comparing two teams' schedules from the current date to the end of the year, it will return a number.
+  This number will tell you how many days the two teams play on coinciding dates.
+  You no longer have to sit that extra player on the bench!
+
+### CONFIGURATION INSTRUCTIONS
+
+  ##### Clone project from github repository git@github.com:NateBainer/FantasyPalFramework.git
+
+  ##### Initialize Repository 
+  - GIT INIT
+
+  ##### Install following dependcies
+  Make sure you have the following dependencies installed (these versions or newer) with npm install
+    "bcryptjs": "^2.4.3",
+    "body-parser": "^1.19.2",
+    "bootstrap": "^5.1.3",
+    "cookie-session": "^2.0.0",
+    "ejs": "^3.1.6",
+    "express": "^4.17.3",
+    "jquery": "^3.6.0",
+    "sass": "^1.35.1",
+    "serve-favicon": "^2.5.0"
+
+
+### OPERATING INSTRUCTIONS
+  When you get to the homepage, register an email and a password.
+  Once inside, you will see an empty My Matchups page. In the top part of the webpage, you will see a button: Compare Schedules.
+  Click that.
+  On this page (Compare Schedules), you will choose two teams and input the current date [YYMMDD].
+  Once you submit this information, you will be taken back to My Matchups and you will see the two teams your compared, the date on which you compared their schedules, and the coinciding dates they play on for the rest of the season!  You can use this information to choose between players you want to add to your team, and players you may want to trade away!
+
+### FILE MANIFEST
+  FantasyPalFramework
+    app.js
+    express_server
+    package-lock
+    package
+    README
+  views
+    partials
+      _header
+    urls_error
+    urls_index
+    urls_login
+    urls_new
+    urls_register
+    urls_show
+
+### CONTACT INFORMATION
+  Nate Bain
+  nate.bain9@gmail.com
+
+### CONTACT INFO FOR LICENSE
+  Loopy Lighthouse
+
+### KNOWN BUGS
+  Only bug I've seen is when I switched wifi, but did not replicate when switching again.
+
+### CHANGELOG FOR PROGRAMMERS 
+  git@github.com:NateBainer/FantasyPalFramework.git
--- express_server.js
+++ express_server.js
@@ -1,9 +1,16 @@
+/* eslint-disable camelcase */
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8090; // default port 8080
 const bodyParser = require("body-parser");
 const bcryptjs = require('bcryptjs');
 const cookieSession = require('cookie-session');
+
+// -----------------FOR SOME REASON, COULD NOT LINK HELPERS.JS AND DATABASE.JS WITHOUT ERROR----------------- //
+const schedDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 
 const users = {
   "userRandomID": {
@@ -22,6 +29,22 @@ const getUserByEmail = (email, database) => {
   return Object.values(database).find(user => user.email === email);
 };
 
+const addURL = (longURL, userID, db) => {
+  const dateCreation = new Date();
+  const visitCount = 0;
+  const visitHistory = [];
+  const uVisitCount = 0;
+  const visitorIDList = [];
+  const shortURL = generateRandomString();
+  db[shortURL] = { userID, longURL, dateCreation, visitCount, visitHistory, visitorIDList, uVisitCount };
+  return shortURL;
+};
+
+const generateRandomString = () => {
+  return Math.random().toString(36).substring(6);
+};
+  
+
 const addUser = (email, password) => {
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const id = generateRandomString();
@@ -33,12 +56,174 @@ const addUser = (email, password) => {
   return id;
 };
 
+const schedsForUser = (id) => {
+  let filtered = {};
+  for (let schedID of Object.keys(schedDatabase)) {
+    if (schedDatabase[schedID].userID === id) {
+      filtered[schedID] = schedDatabase[schedID];
+    }
+  }
+  return filtered;
+};
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: [' '],
+  })
+);
+
+app.set("view engine", "ejs");
+
+// --------------------- ALL ROUTES --------------------------- //
+
 // homepage
 app.get("/", (req, res) => {
   if (req.session.user_id) {
-    res.redirect("/urls");
+    res.redirect("/scheds");
   } else {
     res.redirect("/login");
+  }
+});
+
+// URLS
+app.get("/scheds", (req, res) => {
+  let templateVars = {
+    user: users[req.session.user_id],
+    scheds: schedsForUser(req.session.user_id, schedDatabase)
+  };
+  res.render("scheds_index", templateVars);
+});
+
+
+app.post("/scheds", (req, res) => {
+  const userID = req.session.user_id;
+  if (!userID) {
+    let templateVars = {
+      status: 401,
+      message: 'Hey, Fool! You need to login first!',
+    };
+    res.status(401);
+    res.render("scheds_error", templateVars);
+  } else {
+    const longURL = req.body.longURL;
+    const shortURL = addURL(longURL, userID, schedDatabase);
+    res.redirect(`/scheds/${shortURL}`);
+  }
+});
+
+// URLS/NEW
+app.get("/scheds/new", (req, res) => {
+  let templateVars = { user: users[req.session.user_id] };
+  if (templateVars.user) {
+    res.render("scheds_new", templateVars);
+  } else {
+    res.render("scheds_login", templateVars);
+  }
+});
+
+
+app.post("/scheds/new", (req, res) => {
+  console.log(req.body);
+  const longURL = req.body.longURL;
+  const userID = req.session.user_id;
+  const newURL = addURL(longURL, userID, schedDatabase);
+  res.redirect(`/scheds/${newURL}`);
+});
+
+
+// URLS/:SHORTURL
+app.get("/scheds/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const userID = req.session.user_id;
+  if (!schedDatabase[shortURL]) {
+    let templateVars = {
+      status: 404,
+      message: 'TinyURL has not yet been born!!!',
+    };
+    res.status(404);
+    res.render("scheds_error", templateVars);
+  } else {
+    const longURL = schedDatabase[shortURL].longURL;
+    let templateVars = {user: userID, scheds: schedsForUser(userID), longURL, shortURL};
+    res.render("scheds_show", templateVars);
+  }
+});
+
+app.post("/scheds/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = req.body.longURL;
+  const newDate = new Date();
+  if (req.session.user_id === schedDatabase[shortURL].userID) {
+    schedDatabase[shortURL].longURL = longURL;
+    schedDatabase[shortURL].visitCount = 0;
+    schedDatabase[shortURL].visitHistory = [];
+    schedDatabase[shortURL].uVisitCount = 0;
+    schedDatabase[shortURL].visitorIDList = [];
+    schedDatabase[shortURL].dateCreation = newDate;
+    res.redirect(`/scheds/${shortURL}`);
+  } else {
+    let templateVars = {
+      status: 401,
+      message: "Hey! You can't make changes to that TinyURL!!!",
+      user: users[req.session.user_id]
+    };
+    res.status(401);
+    res.render("scheds_error", templateVars);
+  }
+});
+
+// URLS/:SHORTURL/DELETE
+app.post("/scheds/:shortURL/delete", (req,res) => {
+  const shortURL = req.params.shortURL;
+  if (req.session.user_id === schedDatabase[shortURL].userID) {
+    delete schedDatabase[req.params.shortURL];
+    res.redirect("/scheds");
+  } else {
+    let templateVars = {
+      status: 401,
+      message: "Hey! You can't delete that TinyURL!!!",
+      user: users[req.session.user_id]
+    };
+    res.status(401);
+    res.render("scheds_error", templateVars);
+  }
+});
+
+// U/:SHORTURL
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = schedDatabase[shortURL].longURL;
+  const dateVisit = new Date();
+  if (!schedDatabase[shortURL]) {
+    let templateVars = {
+      status: 404,
+      message: "Hey! This TinyURL ain't been born yet!",
+      user: users[req.session.user_id]
+    };
+    res.status(404);
+    res.render("scheds_error", templateVars);
+  } else if (!req.session.user_id) {
+    req.session.user_id = generateRandomString();
+    schedDatabase[shortURL].visitHistory.push([dateVisit,req.session.user_id]);
+    schedDatabase[shortURL].visitCount++;
+    schedDatabase[shortURL].visitorIDList.push(req.session.user_id);
+    schedDatabase[shortURL].uVisitCount++;
+  } else {
+    const visitorId = schedDatabase[shortURL].visitorIDList;
+    schedDatabase[shortURL].visitHistory.push([dateVisit,req.session.user_id]);
+    schedDatabase[shortURL].visitCount++;
+    if (!visitorId.includes(req.session.user_id)) {
+      visitorId.push(req.session.user_id);
+      schedDatabase[shortURL].uVisitCount++;
+    }
+  }
+  if (longURL.startsWith("http://")) {
+    res.redirect(longURL);
+  } else {
+    res.redirect(`http://${longURL}`);
   }
 });
 
@@ -48,9 +233,9 @@ app.get("/login", (req, res) => {
     user: users[req.session.user_id]
   };
   if (templateVars.user) {
-    res.redirect("/urls");
+    res.redirect("/scheds");
   } else {
-    res.render("urls_login", templateVars);
+    res.render("scheds_login", templateVars);
   }
 });
 
@@ -64,7 +249,7 @@ app.post("/login", (req,res) => {
       user: users[req.session.user_id]
     };
     res.status(401);
-    res.render("urls_error", templateVars);
+    res.render("scheds_error", templateVars);
   } else if (!bcryptjs.compareSync(password, user.password)) {
     let templateVars = {
       status: 401,
@@ -72,17 +257,17 @@ app.post("/login", (req,res) => {
       user: users[req.session.user_id]
     };
     res.status(401);
-    res.render("urls_error", templateVars);
+    res.render("scheds_error", templateVars);
   } else {
     req.session.user_id = user.id;
-    res.redirect("/urls");
+    res.redirect("/scheds");
   }
 });
 
 // LOGOUT
 app.post("/logout", (req, res) => {
   req.session = null;
-  res.redirect("/urls");
+  res.redirect("/scheds");
 });
 
 // REGISTER
@@ -91,9 +276,9 @@ app.get("/register", (req,res) => {
     user: users[req.session.user_id]
   };
   if (templateVars.user) {
-    res.redirect("/urls");
+    res.redirect("/scheds");
   } else {
-    res.render("urls_register", templateVars);
+    res.render("scheds_register", templateVars);
   }
 });
 
@@ -107,7 +292,7 @@ app.post("/register", (req, res) => {
       user
     };
     res.status(401);
-    res.render("urls_error", templateVars);
+    res.render("scheds_error", templateVars);
     ("so... your email and/or password is missing...");
   } else if (getUserByEmail(email, users)) {
     let templateVars = {
@@ -116,11 +301,11 @@ app.post("/register", (req, res) => {
       user: users[req.session.user_id]
     };
     res.status(409);
-    res.render("urls_error", templateVars);
+    res.render("scheds_error", templateVars);
   } else {
     const user_id = addUser(email, password, users);
     req.session.user_id = user_id;
-    res.redirect("/urls");
+    res.redirect("/scheds");
   }
 });
 
